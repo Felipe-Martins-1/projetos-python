860219982a7526b0da995e8b7b81fad93732921a
Felipe <felipe.martins83@etec.sp.gov.br>
2021-07-01 19:27:56 -0300

    Arquivo atualizado: MensagemSegura

---
+++ MensagemSegura/Descricao.md
@@ -0,0 +1,9 @@
+# PythonProjects
+OBSERVAÇÃO: o algoritmo não aceita palavras com acentuação!
+
+Embreve disponibilizarei o programa de gerar as chaves!
+
+Por enquanto, as chave para testar o programa são:
+    - Chave N: 187;
+    - Chave E: 7;
+    - Chave D: 23;
--- MensagemSegura/Principal.py
+++ MensagemSegura/Principal.py
@@ -10,32 +10,22 @@ class Principal:
     def loop(self):
         t = Tela()
         tela1, tela2 = t.telaMenu(), None
-
-        # LEITURA E FUNÇÃO DOS EVENTOS
         while True:  
             janela, evento, valor = sg.read_all_windows()
             c = CritografiaRSA()
-
-            # FECHAR A JANELA
             if janela == tela1 and evento == sg.WIN_CLOSED:
                 break
             elif janela == tela2 and evento == sg.WIN_CLOSED:
                 break
-
-            # MENU
             elif janela == tela1 and evento == "cripTela":
                 tela1.hide()
                 tela2 = t.telaCriptografar()
             elif janela == tela1 and evento == "descripTela":
                 tela1.hide()
                 tela2 = t.telaDescriptografar()
-
-            # VOLTAR PARA A JANELA ANTERIOR 
             elif janela == tela2 and evento == "Voltar":
                 tela2.hide()
                 tela1.un_hide()
-            
-            # FUNÇÕES
             elif janela == tela2 and evento == "crip":  
                 c.criptografar(valor["msg"], valor["chvE"], valor["chvN"])
             elif janela == tela2 and evento == "descrip":  
